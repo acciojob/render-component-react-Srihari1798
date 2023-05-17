8d7f2408207495adaee70aecb2171fd28d74386e
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-05-17 22:54:57 +0530

    submitting code-Wed May 17 2023 22:54:56 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React, {Component,useState} from "react";
+import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main">
-        <p>I am learning React. My life is getting better.</p>
+    <div  id="main">
+      <p>I am learning React. My life is getting better.</p>
     </div>
   )
 }
